7e1d939b3e573182f5e6afd3b38f2ba6e3644a31
student <student@students-MacBook-Pro.local>
2018-07-18 14:43:57 +0200

    Added Feature search result comment count

--- src/app/nav/services/search.service.ts
+++ src/app/nav/services/search.service.ts
@@ -36,19 +36,21 @@ export class SearchService {
           }
         });
       });
-      if (count > 0) {
+      if (count > 0 && element.comment.length > 1) {
         matches.push({ displayName: element.song.artist + ' - ' +  element.song.title , value: count, data: element});
       }
     });
     // if they got the same count, they should be compared by views
     matches.sort((a, b) => {
+      let ret = -1;
       if (a.value > b.value) {
-        return -1;
+        ret = -1;
       } else if (a.value === b.value) {
-        return a.data.data[0].statistics.viewCount > b.data.data[0].statistics.viewCount ? -1 : 1;
+        ret = a.data.data[0].statistics.viewCount > b.data.data[0].statistics.viewCount ? -1 : 1;
       } else {
-        return a.value < b.value ? 1 : 0;
+        ret = a.value < b.value ? 1 : 0;
       }
+      return a.data.comment.length < b.data.comment.length ? 1 : ret;
     });
     return matches;
   }
