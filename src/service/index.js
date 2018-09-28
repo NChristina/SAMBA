94f57cc117348a30225846383268fdc7b312cebd
aringot <alexis.ringot@fhstp.ac.at>
2018-09-28 11:34:57 +0200

    Refactored the search service on the server. Filtering at the beginning then sorting by commentCount and viewCount

--- src/service/index.js
+++ src/service/index.js
@@ -15,17 +15,20 @@ module.context.use(function (req, res, next) {
 
 
 function filterQuery(value) {
+    // Keeps only videos that have a commentCount field so the sort does not crash
     let base = ` 
-    Filter `;
+    Filter version.statistics.commentCount`;
      // contains(upper(videos[*].version.snippet.title), upper(${search}));
     let splitted = value.split(/[ .\-_)(]/);
     for (let i = 0; i < splitted.length; i++) {
-        if (i == 0) {
-            base += `contains(upper(videos[*].version.snippet.title), upper("` + splitted[i] + `"))
+        /*if (i == 0) {
+            base += `contains(upper(version.snippet.title), upper("` + splitted[i] + `"))
             `;
-        } else {
-            base += ` and contains(upper(videos[*].version.snippet.title), upper("` + splitted[i] + `"))`;
-        }
+            //base += `contains(upper(videos[*].version.snippet.title), upper("` + splitted[i] + `"))
+        } else {*/
+            base += ` and contains(upper(version.snippet.title), upper("` + splitted[i] + `"))`;
+            //base += ` and contains(upper(videos[*].version.snippet.title), upper("` + splitted[i] + `"))`;
+        //}
     }
     return base;
 }
@@ -38,28 +41,31 @@ router.get('/search/:value', function (req, res) {
     let filter = filterQuery(search);
     let query = `
     for version in VideoMetadata
+    ` + filter + `
+    sort to_number(version.statistics.commentCount) desc, to_number(version.statistics.viewCount) desc
     let song = first(flatten(for r in 1 outbound version matched
-    return (for s in 1 outbound r requestedAbout
-        return s)
-        ))
+        return (for s in 1 outbound r requestedAbout
+            return s)
+            ))
+            
     let comment = flatten(for c in inbound version commentOnVideo
-        return c)
-    let reply = flatten(for c in inbound version commentOnVideo
-        return (for r in outbound c repliedTo
+                            /* To uncomment when changing the data structure
+                            let replies = (for r in inbound c repliedTo
+                                            return r)
+                            return merge(c, {"replies":replies})*/
+                            return c)
+        
+    let reply = flatten(for c in comment
+        return (for r in inbound c repliedTo
             return r))
-    collect a = first(flatten(for r in 1 outbound version matched
-    return (for s in 1 outbound r requestedAbout
-        return (for a in 1 inbound s sang
-                    return a)))),
-            b = first(flatten(for r in 1 outbound version matched
-    return (for s in 1 outbound r requestedAbout
-        return s)
-        )),
-        c = comment,
-        e = song,
-        d = reply into videos
-        ` + filter + `
-        return {"artist" : a, "song" : b, "data" : videos[*].version, "comment" : c, "reply" : d}
+        
+    let artist = first(flatten(return (for a in 1 inbound song sang return a)))
+
+    return {"artist" : [artist],
+        "song" : song, 
+        "data" : [version], 
+        "comment" : comment,
+        "reply": reply}
     `;
     let keys;
     keys = db._query(query);
@@ -69,3 +75,33 @@ router.get('/search/:value', function (req, res) {
 .response(joi.object().required(), 'Search Data Records')
 .summary('Search Records')
 .description('Returns Data with matches to a word');
+
+
+                /* Old query from when Daniel set up the service
+
+                let query = `
+                for version in VideoMetadata
+                ` + filter + `
+                let song = first(flatten(for r in 1 outbound version matched
+                return (for s in 1 outbound r requestedAbout
+                    return s)
+                    ))
+                let comment = flatten(for c in inbound version commentOnVideo
+                    return c)
+                let reply = flatten(for c in inbound version commentOnVideo
+                    return (for r in outbound c repliedTo
+                        return r))
+                collect a = first(flatten(for r in 1 outbound version matched
+                return (for s in 1 outbound r requestedAbout
+                    return (for a in 1 inbound s sang
+                                return a)))),
+                        b = first(flatten(for r in 1 outbound version matched
+                return (for s in 1 outbound r requestedAbout
+                    return s)
+                    )),
+                    c = comment,
+                    e = song,
+                    d = reply into videos
+                    return {"artist" : a, "song" : b, "data" : videos[*].version, "comment" : c, "reply" : d}
+                `;
+                */
