bd44b071cf33d65be91282f9e48dd95901cfbdea
Victor Oliveira <victorajoliveira@gmail.com>
2018-10-29 15:20:17 +0100

    Inconsistent Sentiments
    When algorithms give both positive and negative scores for comments, an "Inconsistent" signal is given instead of the mean sentiment.

--- src/app/nav/dashboard/comment/comment.component.ts
+++ src/app/nav/dashboard/comment/comment.component.ts
@@ -84,12 +84,12 @@ export class CommentComponent implements OnInit {
         html += d.authorDisplayName + ' | ' + this.dateTimeParser(d.publishedAt) + '<b style="float: right; margin-right: 30px;">';
         html +=	d.artist + ' - ' + d.song + '</b></div><p class="comment-text">' + d.text + '</p></div></div>';
         html += '<div class="bottom-comment" style="float: left; margin-left: 15px">';
-        html += d.likeCount;
-        html += ' <img src="../../../../assets/002-like.svg" height="13px" "class="iconStyle" style="margin: 0px 5px -1px 5px">';
-        html += '| <a style="text-decoration: none; cursor: pointer;" class="accordion" id="replyCount_'+ d._key +'">' + d.replyCount + ' ';
+        html += d.likeCount +' <img src="../../../../assets/002-like.svg" height="13px" "class="iconStyle" style="margin: 0px 5px -1px 5px"> | ';
+        html += '<a style="text-decoration: none; cursor: pointer;" class="accordion" id="replyCount_'+ d._key +'">' + d.replyCount + ' ';
         html += '<img src="../../../../assets/001-reply.svg" height="13px" class="iconStyle" style="margin: 0px 5px -1px 5px">';
-        html += '</a> | '+ this.getSentiment(d.analysis.sentiment,"mean")+' | NLTK: '+ this.getSentiment(d.analysis.sentiment,"nltk")+' | TEXTBLOB: '+ this.getSentiment(d.analysis.sentiment,"blob")+' | AFINN: '+ this.getSentiment(d.analysis.sentiment,"afinn")+'  <div class="panelAc" id="replyPanel_'+ d._key +'" style="display: none;"><p>Lorem Ipsum...</p></div>';
-        html += ' ';
+        html += '</a> | ';
+        html += '<a style="text-decoration: none; cursor: pointer;" class="sentBttn" id="sentBttn_'+ d._key +'">'+ this.getSentiment(d.analysis.sentiment,"mean")+'</a> <div class="panelSent" id="panelSent_'+ d._key +'" style="display:none; background-color:#a9a9a9; color:#fff; font-size:0.8em; padding:1px 5px;">NLTK: '+ this.getSentiment(d.analysis.sentiment,"nltk")+' | TextBlob: '+ this.getSentiment(d.analysis.sentiment,"blob")+' | AFINN: '+ this.getSentiment(d.analysis.sentiment,"afinn")+'</div>';
+        html += '<div class="panelAc" id="replyPanel_'+ d._key +'" style="display: none;"><p>Lorem Ipsum...</p></div>';
         html +=	'</div></div></div></div>';
         return html;
         /** 
@@ -138,6 +138,7 @@ export class CommentComponent implements OnInit {
       .renderTitle(false);
     this.commentTable.render();
     this.buildReplyAccordion();
+    this.buildSentimentBox();
   }
 
   onSelectionChange(event: { index: any, value: any }) {
@@ -187,12 +188,14 @@ export class CommentComponent implements OnInit {
   // get sentiment
   private getSentiment (sentiment: any, mode: string) {
     if(sentiment){
-      // checar inconsistencia: contar qts negativos e qts positivos. se os dois contadores forem maior que zero tem inconsistencia das brabas. imprimir inconsistencia ao inves da media.
-      if(mode == "main") return this.printSentiment(sentiment.mainSentiment, true);
+      if(mode == "main") return this.printSentiment(sentiment.mainSentiment, true); 
       else if(mode == "nltk") return this.printSentiment(sentiment.nltk.compound, false);
       else if(mode == "afinn") return this.printSentiment(sentiment.afinn.normalized, false);
       else if(mode == "blob") return this.printSentiment(sentiment.textBlob.polarity, false);
-      else if(mode == "mean") return this.printSentiment(((sentiment.nltk.compound + sentiment.afinn.normalized + sentiment.textBlob.polarity) / 3), true);
+      else if(mode == "mean"){
+        if(this.isIconsistent([sentiment.nltk.compound,sentiment.textBlob.polarity,sentiment.afinn.normalized])) return 'Inconsistent Sentiment <img src="../../../../assets/009-warning.svg" height="13px" "class="iconStyle" style="margin: 0px 5px -2px 5px">';
+        else return this.printSentiment(((sentiment.nltk.compound + sentiment.afinn.normalized + sentiment.textBlob.polarity) / 3), true);
+      } 
     } 
     else return 'No Sentiment <img src="../../../../assets/008-NA.svg" height="13px" "class="iconStyle" style="margin: 0px 5px -2px 5px">';
   }
@@ -207,18 +210,44 @@ export class CommentComponent implements OnInit {
     else if(sentValue == 0) { scoreValue = "Neutral"; iconValue = "005-neu.svg"; } 
     else if(sentValue < 0 && sentValue > -0.5) { scoreValue = "Negative"; iconValue = "004-neg.svg"; } 
     else if(sentValue <= -0.5) { scoreValue = "Very Negative"; iconValue = "003-veryneg.svg"; } 
-    else return "wat " + sentValue;
+    else return "NaN (" + sentValue + ")";
 
     if(icon) return scoreValue + '<img src="../../../../assets/'+ iconValue +'" height="13px" "class="iconStyle" style="margin: 0px 5px -2px 5px">';
     else return scoreValue;
   }
 
+  private isIconsistent (sentValues: any) {
+    let countPos = 0;
+    let countNeg = 0;
+
+    sentValues.forEach((value) => { 
+      if(value > 0) countPos++;
+      else if(value < 0) countNeg++;
+    });
+
+    if (countPos > 0 && countNeg > 0) return true;
+    else return false;
+  }
 
   // converts the published date into a viewable format (looks better :))
   private dateTimeParser (publishedDate: string) {
     const date = publishedDate.split('T')[0];
     const time = publishedDate.split('T')[1].split('.')[0];
     return date + ' ' + time;
+  }
+
+  buildSentimentBox() {
+    const sentBttn = document.getElementsByClassName('sentBttn');
+    for (let i = 0; i < sentBttn.length; i++) {
+      sentBttn[i].addEventListener('click', function() {
+        const box = document.getElementById('panelSent_' + (this.id.split('_')[1]));
+        if (box.style.display === 'inline') {
+            box.style.display = 'none';
+        } else {
+            box.style.display = 'inline';
+        }
+      });
+    }
   }
 
   buildReplyAccordion() {
