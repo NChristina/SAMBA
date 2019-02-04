fea102d2c1f05445de36f8184bd9d86f98483e3f
Maggie <m.steinacker@gmx.at>
2019-02-04 11:17:55 +0100

    fixed search list problem
    element which was dismissed from the selection list now won't be pushed to the end but to the right index

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -62,6 +62,7 @@ export class NavComponent implements OnInit {
       this.selectedList.forEach((song, i) => {
         if (song.data[0]._id === this.searchMatchList[index].data.data[0]._id) {
           this.selectedList.splice(i, 1);
+          console.log('this.selectedList: ', this.selectedList);
         }
       });
     }
@@ -71,25 +72,16 @@ export class NavComponent implements OnInit {
 
   // removes a song, which was already selected
   removeSelectedSong(index) {
+    console.log('triggered')
     const song = this.selectedList[index];
     this.searchMatchList.forEach((songData, i) => {
       if (songData.data.data[0]._id === song.data[0]._id) {
 
-        let newT = this.searchMatchList[i];
-        this.searchMatchList.splice(i, 1);
-        this.searchMatchList.splice(i, 0, newT);
-        // this.searchMatchList.push({displayName: song.song.artist + ' - ' + song.song.title, data: song});
+        this.searchMatchList[i] = {displayName: song.song.artist + ' - ' + song.song.title, data: song};
+
+        console.log('removeSelectedSong: this.serachMatchList: ', this.searchMatchList);
       }
     });
-    // sorts the data by views (has to be updated by comments)
-    // the sort is used to keep the list as it is, when removing
-    // and pushing the removed song
-    // this.searchMatchList.sort((b, a) => {
-    //   if (a.data.data[0].statistics === b.data.data[0].statistics) {
-    //     return 0;
-    //   }
-    //   return a.data.data[0].statistics.viewCount > b.data.data[0].statistics.viewCount ? -1 : 1; // Sort by view count of song
-    // });
     this.selectedList.splice(index, 1);
     this.chartService.SetData(this.selectedList);
   }
