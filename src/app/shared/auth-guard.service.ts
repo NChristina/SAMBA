9b89c1e4bc68cb996529a615f212330535c3be96
Maggie <m.steinacker@gmx.at>
2019-05-20 14:17:54 +0200

    router guard now active - nav component is protected

---
+++ src/app/shared/auth-guard.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ChartService } from '../nav/dashboard/services/chart.service';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private chartS: ChartService, private router: Router) {
+
+  }
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    console.log('true? : ', this.chartS.getLoggedIn())
+    if (this.chartS.getLoggedIn()) {
+        return true;
+    }
+    // navigate to login page
+    this.router.navigate(['']);
+    // you can save redirect url so after authing we can move them back to the page they requested
+    return false;
+  }
+}
